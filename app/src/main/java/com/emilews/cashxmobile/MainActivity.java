6da285c4b24a21ba33848d4e2a5e2cb127218e59
Emilio Wong <emilews.ew@gmail.com>
2019-08-25 16:28:13 -0700

    Refactored Home fragment

--- app/src/main/java/com/emilews/cashxmobile/MainActivity.java
+++ app/src/main/java/com/emilews/cashxmobile/MainActivity.java
@@ -1,8 +1,13 @@
 package com.emilews.cashxmobile;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.bitcoin.slpwallet.Network;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,7 +15,11 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bitcoin.slpwallet.SLPWallet;
+
 public class MainActivity extends AppCompatActivity {
+    private String DATAFILENAME = "store.csi";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+    public void loadData(){
+        try {
+            FileOutputStream fos = openFileOutput(DATAFILENAME, Context.MODE_PRIVATE);
+        } catch (FileNotFoundException e) {
+            createData();
+        }
+    }
+    public void createData(){
+        SLPWallet wallet = SLPWallet.Companion.fromMnemonic();
     }
 
 }
